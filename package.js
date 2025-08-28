61975961cfda5ffd765c062470c7f345bd33558e
RikChatBot <rediaffan770@gmail.com>
2025-08-28 09:14:51 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "wa-voice-bot",
+  "version": "1.0.0",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.3.1",
+    "twilio": "^4.20.0"
+  }
+}
